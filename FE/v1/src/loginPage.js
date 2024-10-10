f44e93f35017b8d7dd12b23c49d81f43b60e3aa7
charuy2 <109783658+charuy2@users.noreply.github.com>
2024-10-09 21:56:44 -0500

    login page content

--- FE/v1/src/loginPage.js
+++ FE/v1/src/loginPage.js
@@ -1,7 +1,31 @@
+import './loginPage.css';
+import { FaLock } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
+
 const loginPage = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>Sign Up</div>
+                <div className='underline'></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <div className="img"><MdOutlineMailOutline /></div>
+                    <input type="email"/>
+                </div>
+                <div className="input">
+                    <FaLock />
+                    <input type="password"/>
+                </div>
+            </div>
+            <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+            <div className="submit-Container">
+                <div className="submit">Sign Up</div>
+                <div className="submit">Login</div>
+
+            </div>
+
         </div>
     )
 }
