6af223cad92170f5ab715c0636f0c531e92e3f49
ameenchougle <ameenchougle@gmail.com>
2024-10-13 16:40:36 -0500

    putting logo on page

--- FE/v1/src/Nav.js
+++ FE/v1/src/Nav.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import './Nav.css';
+import logo from './TOC Logo.png'
 
 const Nav = () => {
     return (
         //Link option is not working, href works
         <nav className="Nav">
         <ul>
+            <img src = {logo}></img>
             <li><Link to="/login">Login</Link></li> 
             <li><Link to="/main">Main</Link></li>
             <li><Link to="/preferences">Preferences</Link></li>
