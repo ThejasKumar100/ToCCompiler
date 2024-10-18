ffd0977fdda27e4e8a74a7d0bf1d580a5bf60c25
ameenchougle <ameenchougle@gmail.com>
2024-10-17 23:28:12 -0500

    nav and main page style

--- FE/v1/src/Nav.js
+++ FE/v1/src/Nav.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import './Nav.css';
-import logo from './Logo2.png'
+import logo from './Logo.png'
 
 const Nav = () => {
     return (
         //Link option is not working, href works
         <nav className="Nav">
         <ul>
-            <img src = {logo}></img>
+            <img src = {logo}/>  
             <li><Link to="/login">Login</Link></li> 
             <li><Link to="/main">Main</Link></li>
             <li><Link to="/preferences">Preferences</Link></li>
--- FE/v1/src/mainPage.js
+++ FE/v1/src/mainPage.js
@@ -1,6 +1,11 @@
+import logo from './Logo.png'
+import './main.css';
+
+
 const MainPage = () => {
     return (
         <div>
+            <img src = {logo}/> 
             <h1> Welcome to the ToCCompiler </h1>
         </div>
     )
